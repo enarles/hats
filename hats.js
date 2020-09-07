543bd76f027e23f139521b30ada6c8e1e8e583ce
Frederic Ruget <fred@atlant.is>
2020-09-07 23:38:16 +0200

    Pass URL encoded parameter to worker

--- hats.js
+++ hats.js
@@ -22,6 +22,8 @@
 var parameters = {}
 location.search.slice(1).split("&").forEach( function(key_value) { var kv = key_value.split("="); parameters[kv[0]] = kv[1]; })
 console.log(parameters);
+var query = decodeURI(parameters['query']);
+console.log("query is: " + query);
 
 // == HATS CALCULATOR =========================================================
 
